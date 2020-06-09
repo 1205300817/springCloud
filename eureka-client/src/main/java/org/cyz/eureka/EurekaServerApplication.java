d15100556f8deb34353e9c380e0c6c960b2d394a
chengyz <1205300817@qq.com>
2020-06-10 01:12:29 +0800

    eureka server/client 测试

--- eureka-client/src/main/java/org/cyz/eureka/EurekaServerApplication.java
+++ eureka-client/src/main/java/org/cyz/eureka/EurekaServerApplication.java
@@ -2,9 +2,10 @@ package org.cyz.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaServerApplication {
 
---
+++ eureka-client/src/main/java/org/cyz/eureka/controller/HelloController.java
@@ -0,0 +1,36 @@
+package org.cyz.eureka.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private DiscoveryClient client;
+
+    private final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public Object index(){
+        Map<String, List<ServiceInstance>> msl = new HashMap<>();
+        List<String> services = client.getServices();
+        for (String service : services) {
+            List<ServiceInstance> sis = client.getInstances(service);
+            msl.put(service, sis);
+            log.info("service: {}",service);
+        }
+        return msl;
+    }
+
+}
